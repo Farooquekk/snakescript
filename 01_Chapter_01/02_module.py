561a3cbbb22df7584be9462a05e835950ce2bbf5
Farooquekk <22sw040@students.muet.edu.pk>
2025-06-18 06:13:45 -0700

    Starting Python
    print statement and modules.

---
+++ 01_Chapter_01/02_module.py
@@ -0,0 +1,10 @@
+import pyjokes
+joke = pyjokes.get_joke()
+# print("Printing a Joke")
+# This will praint a random joke
+print(joke)
+
+# Multi line comment
+"""
+thank you for using the pyjokes module.
+"""
