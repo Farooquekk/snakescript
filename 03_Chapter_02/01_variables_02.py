8c59e86dc277bd7b20e7ca52553e06234b158b85
Farooquekk <22sw040@students.muet.edu.pk>
2025-06-24 06:08:49 -0700

    Rename and add variable assignment examples
    Renamed 01_variables.py to 01_variables_01.py for clarity. Added 01_variables_02.py with examples of multiple variable assignments and variable deletion in Python.

---
+++ 03_Chapter_02/01_variables_02.py
@@ -0,0 +1,22 @@
+#  declaring multiple variables but assigning each variable same value.
+
+# a = b = c =10
+
+# print('a',a)
+# print('b',b)
+# print('c',c)
+
+#  declaring multiple variables but assigning each variable different value.
+
+# a,b,c = 1,2,3
+
+# print('a',a)
+# print('b',b)
+# print('c',c)
+
+# deleting a varaible
+
+a = 40
+print('a',a)
+del a
+print('a',a) # will give name error a is not defined
